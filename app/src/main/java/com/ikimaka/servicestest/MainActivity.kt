ddf569b3ae9a37f15f004d05b2dff7604daacb60
ikimaka <ikimaka@yandex.ru>
2024-05-29 13:38:44 +0300

    Initial commit

---
+++ app/src/main/java/com/ikimaka/servicestest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ikimaka.servicestest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ikimaka.servicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
